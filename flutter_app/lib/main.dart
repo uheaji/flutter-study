4303f41ff17d29486c7b64053ec8ed8db2b54b18
uheaji <yoolbk@naver.com>
2021-12-20 12:10:21 +0900

    [6] Text

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -7,37 +7,21 @@ void main() {
 
 class FirstApp extends StatelessWidget {
 
-  final colorCodes = [600, 500, 400, 300, 200];
-  final data = ["가", "나", "다", "라", "마"];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          body: ListView.separated(
-            itemCount: 5,
-            itemBuilder: (BuildContext context, int index){
-              return newMethod(index); // 메소드로 추출
-            },
-            separatorBuilder: (context, index) => Divider(), // 리스트 구분선이 생긴다! (기본)
+          body: Center(
+            child: Image.asset(
+              "dinotaeng.jpg",
+              width: 200,
+              // height: 500,
+              // fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
     );
   }
-
-
-  Widget newMethod(int index) {
-    return ListTile( // 특별한 디자인을 만들고 싶으면 row, column으로 새로 만들어야한다.
-      title: Text("제목"),
-      subtitle: Text("부제목"),
-      leading: Icon(Icons.person),
-      onTap: () {
-        print("눌러짐");
-      },
-    );
-  }
 }
-
-
