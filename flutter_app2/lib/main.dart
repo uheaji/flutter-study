7eef587fb34c6e2e27adcec0aa68c722a52b104f
uheaji <yoolbk@naver.com>
2021-12-27 14:07:44 +0900

    [20211227] 비만도 계산기

--- flutter_app2/lib/main.dart
+++ flutter_app2/lib/main.dart
@@ -8,60 +8,183 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '폼 검증 데모',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('폼 검증 데모'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BmiMain(),
+    );
+  }
+}
+
+class BmiMain extends StatefulWidget {
+  @override
+  _BmiMainState createState() => _BmiMainState();
+}
+
+class _BmiMainState extends State<BmiMain> {
+  // 폼의 상태를 얻기 위한 키
+  final _formKey = GlobalKey<FormState>();
+
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('비만도 계산기'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          // 키 할당
+          key: _formKey,
+          child: Column(
+            children: [
+              // 키 입력 필드
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '키',
+                ),
+                keyboardType: TextInputType.number,
+                controller: _heightController,
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    // 입력한 값의 앞뒤 공백을 제거한 것이 비었다면 에러 메시지 표시
+                    return '키를 입력하세요';
+                  }
+                  return null; // null을 반환하면 에러가 없는것임
+                },
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              // 몸무게 입력 필드
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '몸무게',
+                ),
+                keyboardType: TextInputType.number,
+                controller: _weightController,
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return '키를 입력하세요';
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16.0),
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 폼에 입력된 값 검증
+                    if (_formKey.currentState!.validate()) {
+                      // 검증시 처리
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => BmiResult(
+                                height:
+                                    double.parse(_heightController.text.trim()),
+                                weight: double.parse(
+                                    _weightController.text.trim()))),
+                      );
+                    }
+                  },
+                  child: Text('결과'),
+                ),
+              )
+            ],
+          ),
         ),
-        body: MyCustomForm(),
       ),
     );
   }
 }
 
-class MyCustomForm extends StatefulWidget {
-  @override
-  State<MyCustomForm> createState() => _MyCustomForm();
-}
+class BmiResult extends StatelessWidget {
+  // 상수는 값을 반드시 초기화해야 하므로 생성자를 통해 초기화가 이루어진다.
+  final double height; // 키
+  final double weight; // 몸무게
 
-class _MyCustomForm extends State<MyCustomForm> {
-  // Form 위젯에 유니크한 키값을 부여하고 검증시 사용
-  final _formKey = GlobalKey<FormState>();
+  BmiResult({
+    required this.height,
+    required this.weight,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // Form 위젯에 _formKey를 지정
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // TextFormField위젯은 TextField 위젯이 제공하는 기능에 추가로 validator 프로퍼티를 활용한 검증 기능도 제공한다.
-          TextFormField(
-            // validator에는 입력된 값(valuie)를 인수로 받는 함수를 작성합니다.
-            validator: (value) {
-              if (value!.isEmpty) {
-                return '글자를 입력하세요.';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // 폼을 검증하여 통과하면 true, 실패하면 false 리턴
-                if (_formKey.currentState!.validate()) {
-                  // 검증이 통과하면 스낵바 표시
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text('검증 완료')));
-                }
-              },
-              child: Text('검증'),
+    final bmi = weight / ((height / 100) * (height / 100));
+    print('bmi : $bmi');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('비만도 계산기'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _calcBmi(bmi),
+              style: TextStyle(fontSize: 36),
             ),
-          )
-        ],
+            SizedBox(
+              height: 16,
+            ),
+            _buildIcon(bmi),
+          ],
+        ),
       ),
     );
+  }
+
+  String _calcBmi(double bmi) {
+    var result = '저체중';
+    if (bmi >= 35) {
+      result = '고도 비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과제충';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+    return result;
+  }
+
+  Widget _buildIcon(double bmi) {
+    if (bmi >= 23) {
+      return Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.red,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      return Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    } else {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.orange,
+        size: 100,
+      );
+    }
   }
 }
