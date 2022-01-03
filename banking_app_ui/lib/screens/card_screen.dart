a741e2525abbf9375c8428029de160bc765d0d19
uheaji <yoolbk@naver.com>
2022-01-03 16:44:27 +0900

    [bankingApp UI]  4. 완성

--- banking_app_ui/lib/screens/card_screen.dart
+++ banking_app_ui/lib/screens/card_screen.dart
@@ -1,3 +1,7 @@
+import 'package:banking_app_ui/constants/app_textstyle.dart';
+import 'package:banking_app_ui/constants/color_constants.dart';
+import 'package:banking_app_ui/data/card_data.dart';
+import 'package:banking_app_ui/widgets/my_card.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -5,6 +9,66 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'My Cards',
+          style: TextStyle(
+            fontFamily: "Poppins",
+            color: kPrimaryColor,
+          ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage:
+                NetworkImage("https://placeimg.com/640/480/people"),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_active_outlined,
+              color: Colors.black,
+              size: 27,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                return MyCard(card: myCards[index]);
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 20);
+              },
+              itemCount: myCards.length,
+              shrinkWrap: true,
+            ),
+          ),
+          CircleAvatar(
+            radius: 30,
+            child: Icon(
+              Icons.add,
+              size: 40,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "ADD CARD",
+            style: ApptextStyle.LISTTILE_TITLE,
+          )
+        ],
+      ),
+    );
   }
 }
